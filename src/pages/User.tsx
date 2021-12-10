a5630edb8434a5448c6255a2fd456efd12e96bce
daniel.mh.chan <daniel.mh.chan@gtomato.com>
2021-12-10 18:31:05 +0800

    - applied logic for setting user data in profile page by url path

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+import { RootState } from "..";
 import { ContentWrap, PageContentTop, UserPerRowButton, PageWrap } from "./style"
 
 export default function User() {
     const location = useLocation();
+    const [currentUser, setCurrentUser] = useState(location.pathname.split('/')[2]);
+    const [userProfile, setUserProfile] = useState([]);
+    const userData = useSelector((state:RootState) => state.userData);
+
+    useEffect(() => {
+        setUserProfile(userData.filter((it: any) => it.login.uuid === currentUser))
+    }, [currentUser, userData])
     
     return(
         <PageWrap>User</PageWrap>
